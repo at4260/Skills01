a28652290f9460bde927b664c8e3e48d237629e9
Hackbright Student <info@hackbrightacademy.com>
2015-01-20 19:01:30 -0800

    finished

--- skill_1_lab.py
+++ skill_1_lab.py
@@ -43,33 +43,99 @@ print long_words(word_list)
 
 # Write a function that finds the smallest element in a list of integers and returns it.
 def smallest(number_list):
-    number_list.sorted()
-    number_list[0]
+
+    init_val = number_list[0]
+
+    for each_number in number_list:
+        if each_number < init_val:
+            init_val = each_number
+        else:
+            continue
+    return init_val
+
+
+    # number_list.sort()
+
+    # return number_list[0]
+
+print smallest(number_list)
+
 
 # Write a function that finds the largest element in a list of integers and returns it.
 def largest(number_list):
-    return None
+    
+    sort_number = sorted(number_list)
+
+    return sort_number[-1]
+
+print largest(number_list)
 
 # Write a function that takes a list of numbers and returns a new list of all those numbers divided by two.
 def halvesies(number_list):
-    return []
+
+    new_list=[]
+
+    for each_number in number_list: 
+        num_float = float(each_number)
+        each_number = (num_float/2)
+        new_list.append(each_number)   
+
+    return new_list
+
+print halvesies(number_list)  
+
 
 # Write a function that takes a list of words and returns a list of all the lengths of those words.
 def word_lengths(word_list):
-    return []
+    new_word_list = []
+
+    for each_word in word_list:
+        length_of_word=len(each_word)
+        new_word_list.append(length_of_word)
+
+    return new_word_list
+
+print word_lengths(word_list)
+
 
 # Write a function (using iteration) that sums all the numbers in a list.
 def sum_numbers(number_list):
-    return 0
+    sums = 0
+    for each_number in number_list:
+        sums += each_number
+    return sums
+
+print sum_numbers(number_list)
 
 # Write a function that multiplies all the numbers in a list together.
 def mult_numbers(number_list):
-    return 0
+    multiplies = 1
+    for each_number in number_list:
+        multiplies *= each_number
+    return multiplies
+
+print mult_numbers(number_list)
+
 
 # Write a function that joins all the strings in a list together (without using the join method) and returns a single string.
 def join_strings(word_list):
-    return ""
+    sent = ""
+    for each_word in word_list:
+        right_word = each_word + " "
+        sent += right_word
+    return sent
+
+print join_strings(word_list)
 
 # Write a function that takes a list of integers and returns the average (without using the avg method)
 def average(number_list):
-    return 0
+    sums = 0
+    count = 0
+
+    for each_num in number_list:
+        sums += float(each_num)
+        count += 1
+        avg = float(sums/count)
+    return avg
+
+print average(number_list)
